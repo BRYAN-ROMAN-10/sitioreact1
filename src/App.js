c8603a7a1e993ca0994ff019b8f5c9fa6505b845
Bryan Roman <bryanroca2000@gmail.com>
2022-12-03 12:28:24 -0500

    V04, Implementacion del formulario completo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { collection, getDocs, query, doc, deleteDoc, where, } from "firebase/firestore";
+import { collection, getDocs, query, doc, deleteDoc, where, onSnapshot, } from "firebase/firestore";
 //import {getDoc, addDoc, updateDoc, setDoc, increment } from "firebase/firestore";
 import React, { useEffect,useState } from 'react';
 import firebase, { db } from './componente/firebase';
 import AppForm from './componente/AppForm';
+import {toast} from 'react-toastify';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 //import { async } from "@firebase/util";
 
 function App() {
@@ -14,31 +17,102 @@ function App() {
   const [orden,setOrden] = useState(0);            //Para número - falla
   const i = 1;                                     //Para número - falla
 
+  console.log(docsBD);
+
   const fnRead = async () =>{
+    try {
+      const xColeccionConQuery = query(collection(db, "persona"));
+      const unsubscribe = onSnapshot(xColeccionConQuery, (xDatosBD) => {
+        const xDoc = [];
+        xDatosBD.forEach( (doc) => {
+          xDoc.push({id: doc.id, ...doc.data()});
+          //console.log({id: doc.id, ...doc.data()});
+        });
+        setDocsBD(xDoc)
+        console.log(docsBD);
+      });
+    } catch (error) {
+      console.error(error);
+    }
 
   }
 
   useEffect( () => {
-    
+    fnRead();
   }, [idActual])
 
   ///////////////////////////////////////////////////////////////
   ////////////////////DELETE - fnDelete - Eliminar///////////////
   ///////////////////////////////////////////////////////////////
   const fnDelete = async (xId) => {
-
+    if(window.confirm("Confirme para eliminar")){
+      await deleteDoc(doc(db, 'persona', xId));
+      toast("Documento eliminado con exito", {
+        type:'error',
+        autoClose:2000
+      })
+    }
   }
 
   return (
-    <div style={{width:"350px", background:"greenyellow", padding:"10px"}} >
+    <div className="container text-center">
+      <div className="card bs-secondary p-3 mt-3">
+        <ToastContainer/>
+        <div className="col-md-12 p-2">
+          <div className="card mb-1">
+            <h1>Sitioreact1-CRUD (App.js)</h1>
+          </div>
+        </div>
+
+        <div className="col-md-12 p-2">
+          <AppForm {...{idActual, setIdActual}} />
+        </div>
+
+        <div className="col-md-12 p-2">
+        {
+          docsBD.map( (row) =>
+            <div className="card mb-1" key={row.id}>
+              <div className="card-body">
+                <div className="d-flex justify-content-between">
+                  <h4>N.  -{row.nombre}</h4>
+                  <div>
+                    <i className="material-icons text-danger"
+                      onClick={() => fnDelete(row.id)}>close</i>
+                      ...
+                    <i className="material-icons text-danger"
+                      onClick={() => setIdActual(row.id)}>create</i>
+                  </div>
+                </div>
+                <div className="d-flex justify-content">
+                  <span>Edad: {row.edad} </span>
+                  <a href="#"> Genero: {row.genero}</a>
+                </div>
+              </div>
+            </div>
+          )
+        }
+        </div>
+      </div>
+    </div>
+
+
+
+
+/*    <div style={{width:"350px", background:"greenyellow", padding:"10px"}} >
       <h1>REGISTER</h1>
       <h1>Sitio de Bryan Roman (App.Js)</h1>
       <AppForm {...{idActual, setIdActual, fnRead}} />
       {
-        //docsBD.map( (p) => 
-        //)
+        docsBD.map( (row) => 
+          <p key={row.id}> 
+            No. 1 {row.nombre} ...
+            <span onClick={() => fnDelete(row.id)}>X</span>
+            ...
+            <span onClick={() => setIdActual(row.id)}>A</span>
+          </p>
+        )
       }
-    </div>
+    </div>*/
   );
 }
 
--- src/componente/AppForm.js
+++ src/componente/AppForm.js
@@ -3,6 +3,8 @@ import {collection, doc, getDoc, addDoc, updateDoc} from "firebase/firestore";
 import React, { useEffect, useState } from 'react';
 import firebase, { db } from './firebase';
 import { async } from '@firebase/util';
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AppForm = (props) => {
     ///////////////////////////////////////////////////////////////
@@ -11,28 +13,44 @@ const AppForm = (props) => {
     const camposRegistro = {nombre:"", edad:"", genero:""};   //estructura tbL
     const [objeto, setObjeto] = useState(camposRegistro);     //Tabla u objeto
 
-    const hanleStatuschange = (e) => {                        //Manejo cambios en input...
+    const handleStatusChange = (e) => {                        //Manejo cambios en input...
         const {name, value} = e.target;                       //Capta lo que se escribe
         setObjeto({...objeto, [name]:value });                //Asigna al objeto name y value
         //console.log(objeto);                                //Ver en tiempo real
     }
 
     const handleSubmit = async (e) => {                       //Manejo submit (emvio)
-        e.preventDefault();                                   //evitar por defecto (false)
+                                        //evitar por defecto (false)
         //////////REGISTRAR////////////////////////////////////////
-        if(props.idActual === ""){
-            //console.log(props.idActual);                    //Verificar idActual
-            if(validarForm()){                                //Verificar
-                addDoc(collection(db, 'persona'), objeto);    //CREAR
-                console.log('Se guardo...');                  //Msj
-                props.fnRead();                               //Actualizar LECTURA BD
+        try{
+            e.preventDefault();
+            if(props.idActual === ""){
+                //console.log(props.idActual);                    //Verificar idActual
+                if(validarForm()){                                //Verificar
+                    addDoc(collection(db, 'persona'), objeto);    //CREAR
+                    //console.log('Se guardo...');                  //Msj
+                    toast("se GUARDO con exito...", {
+                        type:'success',
+                        autoClose: 2000
+                    })
+                    //props.fnRead();                               //Actualizar LECTURA BD
+                }else{
+                    console.log('No se guardo...');
+                }
             }else{
-                console.log('No se guardo...');
+                await updateDoc(doc(collection(db, "persona"), props.idActual), objeto);
+                //console.log("Se actualizó... ");
+                toast("se ACTUALIZO con exito...", {
+                    type:'info',
+                    autoClose: 2000
+                })
+                //props.fnRead();           //No es necesario se cambio fn en useEffect
+                props.setIdActual('');
             }
-        }else{
-
+            setObjeto(camposRegistro); 
+        } catch (error) {
+            console.log("Error en CREAR o UPDATE: ", error);
         }
-        setObjeto(camposRegistro);                             //Limpiar objeto
     }
     /////////////////////////////VALIDACIÓN////////////////////////
     const validarForm = () => {
@@ -47,29 +65,53 @@ const AppForm = (props) => {
     ///////////////////////////////////////////////////////////////
     //console.log("props.idActual", props.idActual);
     useEffect(() => {
-
+        if(props.idActual === ""){
+            setObjeto({...camposRegistro});
+        }else{
+            obtenerDastosPorId(props.idActual);
+        }
     },[props.idActual]);
 
     const obtenerDastosPorId = async (xId) =>{
-        
+        const objPorId = doc(db, "persona", xId);
+        const docPorId = await getDoc(objPorId);
+        if (docPorId.exists()) {
+            setObjeto(docPorId.data());
+        }else{
+            console.log("No hay doc... ");
+        }
     }
     //console.log(objeto);
 
     return (
-        <div style={{background:"orange", padding:"10px", textAlign:"center"}}>
-        <h1>AppForm.js</h1>
-        <form onSubmit={handleSubmit}>
-            <input type="text" name="nombre" placeholder="Nombres..."
-            onChange={hanleStatuschange} value={objeto.nombre} /> <br/>
-
-            <input type="text" name="edad" placeholder="Edad..."
-            onChange={hanleStatuschange} value={objeto.edad} /> <br/>
-
-            <input type="text" name="genero" placeholder="Género..."
-            onChange={hanleStatuschange} value={objeto.genero} /> <br/>
-
-            <button>
-                {props.idActual === ""? "Guardar" : "Actualizar"}
+        <div>
+        <form className="card card-body" onSubmit={handleSubmit}>
+            <button className="btn btn-primary btn-block"> 
+                Formulario (AppForm.js)
+            </button>
+            <div className="form-group input-group">
+                <div className="input-group-text bd-light">
+                    <i className="material-icons">group_add</i>
+                </div>
+                <input type="text" className="form-control" name="nombre" placeholder="Nombres..."
+                    onChange={handleStatusChange} value={objeto.nombre} />
+            </div>
+            <div className="form-group input-group clearfix">
+                <div className="input-group-text bd-light ">
+                    <i className="material-icons">star_half</i>
+                </div>
+                <input type="text" className="form-control float-start" name="edad" placeholder="Edad..."
+                    onChange={handleStatusChange} value={objeto.edad} />
+            </div>
+            <div className="form-group input-group">
+                <div className="input-group-text bd-light">
+                    <i className="material-icons">insert_link</i>
+                </div>
+                <input type="text" className="form-control" name="genero" placeholder="Género..."
+                    onChange={handleStatusChange} value={objeto.genero} />
+            </div>
+            <button className="btn btn-primary btn-block"> 
+                {props.idActual === ""? "Guardar" : "Actualizar"} 
             </button>
         </form>
         </div>
